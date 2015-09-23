8e4c291acfaba69a3ee314b37c980cd8c2edfda4
John <John@Johns-MacBook-Pro.local>
2015-09-22 21:22:01 -0300

    Initial Commit

---
+++ bmi calcc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bmi calcc
+//
+//  Created by John on 2015-09-22.
+//  Copyright (c) 2015 John. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
